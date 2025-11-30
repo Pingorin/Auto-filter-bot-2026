0421674a848def590074f8bae692a8fb11896051
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-11-30 21:29:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Step 1 से प्राप्त की गई जानकारी यहां भरें
+class Config:
+    # 1. Telegram API Details
+    API_ID = int("20638104")
+    API_HASH = "6c884690ca85d39a4c5ad7c15b194e42"
+    
+    # 2. Bot Token
+    BOT_TOKEN = "8504476517:AAHfyOVpPuWyWtgh-mY4Uh6zIoSK7J1CbkI"
+    
+    # 3. Admin User IDs (आपकी ID - इसे /get_id बॉट से प्राप्त कर सकते हैं)
+    ADMINS = [int("7245547751")]
+    
+    # 4. Filter Channel IDs (फ़िलहाल खाली छोड़ दें)
+    # बाद में, जिन चैनल्स को बॉट इंडेक्स करेगा, उनकी ID यहाँ भरेंगे
+    CHANNELS = [] 
