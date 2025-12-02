abbd124825e65490b7dd839533a3a5b829f93dd0
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-12-02 09:43:06 +0530

    Update and rename info.py to config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# BotFather se mila hua API Token yahan dalein
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+
+# Aapke bot ka username (bina @ ke), example: "MySuperBot"
+# Yeh "Add me to your groups" link ke liye zaruri hai
+BOT_USERNAME = "YourBotUsername"
+
+# Links jo buttons par click karne se khulenge
+CHANNEL_LINK = "https://t.me/YourChannelLink"
+OWNER_LINK = "https://t.me/YourUsername"
--- info.py
+++
@@ -1,9 +0,0 @@
-# info.py (Correct way to use environment variables)
-import os
-# ...
-class Config:
-    API_ID = int(os.environ.get("API_ID", "123456")) 
-    API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN") 
-    DATABASE_URI = os.environ.get("DATABASE_URI", "YOUR_MONGODB_URI")
-    # ...
