6d87e860b0cec304fa75b069ba156859d13857c1
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-12-02 10:30:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
 # BotFather se mila hua token yahan dalein
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8504476517:AAHfyOVpPuWyWtgh-mY4Uh6zIoSK7J1CbkI"
 
 # MongoDB connection url (MongoDB Atlas ya local)
-MONGO_URL = "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URL = "mongodb+srv://Anu55:Anu55@cluster0.8ohtzju.mongodb.net/?retryWrites=true&w=majority"
 
 # Apne channel aur owner ka link
 CHANNEL_LINK = "https://t.me/YourChannelLink"
