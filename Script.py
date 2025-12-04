ce4cfce4ec4f047318b974d1e7dcc7b6bbcf1046
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-12-04 22:10:20 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,13 @@
+class script:
+    START_TXT = """<b>👋 Hello {0}!</b>
+
+I am an advanced group management bot.
+
+<b>📊 Your Status:</b> {1}
+<b>🆔 Your ID:</b> <code>{2}</code>
+
+Click the buttons below to continue."""
+
+# Temp variable storage
+class temp:
+    U_NAME = None
