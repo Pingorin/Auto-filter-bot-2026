dd8a9cbaf5fc6053354704968dc7b2b927579ecc
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-12-06 22:42:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,11 +1,36 @@
 from pyrogram import Client, filters
-from database.users_chats_db import db
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from utils import temp  # Username lene ke liye
 
-@Client.on_message(filters.command("start"))
+# 1. Yahan apni photo ka direct link dalein
+# (Aap Telegraph graph.org ya kisi bhi direct image link ka use kar sakte hain)
+START_IMG = "https://graph.org/file/9d2c2084627d70404764b.jpg"
+
+@Client.on_message(filters.command("start") & filters.private)
 async def start(client, message):
-    # Database me user add karo
-    await db.add_user(message.from_user.id)
     
-    await message.reply_text(
-        f"Hello {message.from_user.mention}! 👋\n\nMain ek Auto Filter Bot hu. Abhi main naya bana hu!"
+    # 2. Start Message Text
+    text = f"""Hello {message.from_user.mention} 👋,
+
+Main ek **Auto Filter Bot** hu. 
+Muje apne group me add karo movies aur series provide karne ke liye.
+
+Niche diye gaye buttons check karein 👇"""
+
+    # 3. Aapke Buttons
+    buttons = [[
+        InlineKeyboardButton('⇆ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ⇆', url=f'http://t.me/{temp.U_NAME}?startgroup=start')
+    ],[
+        InlineKeyboardButton('⚙ ꜰᴇᴀᴛᴜʀᴇs', callback_data='features'),
+        InlineKeyboardButton('💸 ᴘʀᴇᴍɪᴜᴍ', callback_data='buy_premium')
+    ],[
+        InlineKeyboardButton('🚫 ᴇᴀʀɴ ᴍᴏɴᴇʏ ᴡɪᴛʜ ʙᴏᴛ 🚫', callback_data='earn'),
+        InlineKeyboardButton('🤝 ʀᴇꜰᴇʀʀᴀʟ 🤝', callback_data='refer')
+    ]]
+    
+    # 4. Message Send Karna (Photo ke saath)
+    await message.reply_photo(
+        photo=START_IMG,
+        caption=text,
+        reply_markup=InlineKeyboardMarkup(buttons)
     )
